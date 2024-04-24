db0b1121a45e569baedb8b59fb76f468692e055c
Marvin Cadiz <xdmarvinzk@gmail.com>
2024-04-24 23:31:19 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+	
+		<title>CADIZMIX!</title>
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" href="css/animate.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/templatemo-style.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.singlePageNav.min.js"></script>
+		<script src="js/typed.js"></script>
+		<script src="js/wow.min.js"></script>
+		<script src="js/custom.js"></script>
+	</head>
+	<body id="top">
+
+		<!-- start preloader -->
+		<div class="preloader">
+			<div class="sk-spinner sk-spinner-wave">
+     	 		<div class="sk-rect1"></div>
+       			<div class="sk-rect2"></div>
+       			<div class="sk-rect3"></div>
+      	 		<div class="sk-rect4"></div>
+      			<div class="sk-rect5"></div>
+     		</div>
+    	</div>
+    	<!-- end preloader -->
+
+        <!-- start header -->
+        <header>
+           
+        </header>
+        <!-- end header -->
+
+    	<!-- start navigation -->
+		<nav class="navbar navbar-default templatemo-nav" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon icon-bar"></span>
+						<span class="icon icon-bar"></span>
+						<span class="icon icon-bar"></span>
+					</button>
+					<a href="#" class="navbar-brand">CADIZ MIX LIGHTS & SOUNDS!</a>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#top">HOME</a></li>
+						<li><a href="#about">ABOUT</a></li>
+						<li><a href="#service">SERVICE</a></li>
+						<li><a href="#contact">CONTACT</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<!-- end navigation -->
+
+    	<!-- start home -->
+    	<section id="home">
+    		<div class="container">
+    			<div class="row">
+    				<div class="col-md-offset-2 col-md-8">
+    					<h1 class="wow fadeIn" data-wow-offset="50" data-wow-delay="0.9s"><span>CADIZ MIX</span> <br> <span>SOUNDS & LIGHTS!</span> </br> </h1>
+    					<div class="element">
+                            <div class="sub-element">Hello, This is a CADIZ MIX!</div>
+                            <div class="sub-element">Your Ultimate Sound and Light Solution!</div>
+                            <div class="sub-element">Bringing Your Events to Life: Sound, Lights, Magic!</div>
+							<div class="sub-element">Setting the Stage for Unforgettable Experiences: Where Sound and Light Meet Brilliance!</div>
+							<div class="sub-element">Illuminate Your Events, Amplify Your Moments: Sound and Light Mastery at Your Service.</div>
+							<div class="sub-element">Your priority is ours.</div>
+                        </div>
+    					<a data-scroll href="#about" class="btn btn-default wow fadeInUp" data-wow-offset="50" data-wow-delay="0.6s">BOOK NOW!</a>
+    				</div>
+    			</div>
+    		</div>
+    	</section>
+    	<!-- end home -->
+
+    	<!-- start about -->
+		<section id="about">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+    					<h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">WHO WE <span>ARE</span></h2>
+    				</div>
+							<div class="media-body">
+								<p>Welcome to CADIZ MIX Lights & Sounds, where we transform events into extraordinary experiences. With cutting-edge sound systems and captivating lighting designs, we craft moments that resonate and illuminate, leaving lasting impressions. Let us bring your vision to life with a symphony of sound and light that captivates and inspires.</p>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+				</div>
+			
+		</section>
+		<!-- end about -->
+
+    	<!-- start service -->
+    	<section id="service">
+    		<div class="container">
+    			<div class="row">
+    				<div class="col-md-12">
+    					<h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">OUR <span>SERVICES OFFERED:</span></h2>
+    				</div>
+    				<div class="col-md-4 wow fadeIn" data-wow-offset="50" data-wow-delay="0.6s">
+    					
+    					<h4>Event Sound Reinforcement:</h4>
+    					<p>Perfect for weddings, conferences, concerts, and other live events, this service provides high-quality audio equipment, including speakers, microphones, mixers, and amplifiers, to ensure crystal-clear sound projection and balanced acoustics throughout the venue.</p>
+    				</div>
+    				<div class="col-md-4 active wow fadeIn" data-wow-offset="50" data-wow-delay="0.9s">
+    					
+    					<h4>DJ and Live Performance Support:</h4>
+    					<p>Tailored for DJs, bands, and performers, this service includes professional sound system setups designed to enhance live performances. It features specialized equipment such as DJ mixers, turntables, stage monitors, and subwoofers, delivering optimal sound quality and control for dynamic performances.</p>
+    				</div>
+    				<div class="col-md-4 wow fadeIn" data-wow-offset="50" data-wow-delay="0.6s">
+    					
+    					<h4>Audiovisual Integration:</h4>
+    					<p>Ideal for corporate events, exhibitions, and multimedia presentations, this service combines sound reinforcement with visual elements for a complete sensory experience. It includes audiovisual equipment such as projectors, screens, LED walls, and lighting fixtures, seamlessly integrated with the sound system to create immersive environments and impactful presentations.</p>
+    				</div>
+    			</div>
+    		</div>
+    	</section>
+    	<!-- end service -->
+
+    	<!-- start contact -->
+    	<section id="contact">
+    		<div class="container">
+    			<div class="row">
+    				<div class="col-md-12">
+    					<h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">Sound <span>Assistance</span></h2>
+    				</div>
+    				<div class="col-md-6 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-offset="50" data-wow-delay="0.9s">
+    					<form action="#" method="post">
+    						<label>NAME</label>
+    						<input name="fullname" type="text" class="form-control" id="fullname">
+   						  	
+                            <label>EMAIL</label>
+    						<input name="email" type="email" class="form-control" id="email">
+   						  	
+                            <label>MESSAGE</label>
+    						<textarea name="message" rows="4" class="form-control" id="message"></textarea>
+    						
+                            <input type="submit" class="form-control">
+    					</form>
+    				</div>
+    				<div class="col-md-6 col-sm-6 col-xs-12 wow fadeInRight" data-wow-offset="50" data-wow-delay="0.6s">
+    					<address>
+    						<p class="address-title">Where We Are Located</p>
+    						<span>Purok 14. Brgy. Poblacion Sta. Elena Camarines Norte.</span>
+    						<p><i class="fa fa-phone"></i> +639 12345 6789</p>
+    						<p><i class="fa fa-envelope-o"></i>CMlights@lcc-daet.edu.ph</p>
+    						<p><i class="fa fa-map-marker"></i> Sta. Elena Camarines Norte.</p>
+    					</address>
+    					<ul class="social-icon">
+    						<li><h4>SOUND WITH US</h4></li>
+    						<li><a href="https://www.facebook.com/profile.php?id=100092707240061" class="fa fa-facebook"></a></li>
+    						
+    				</div>
+    			</div>
+    		</div>
+    	</section>
+    	<!-- end contact -->
+
+        <!-- start copyright -->
+        <footer id="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <p class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s">
+                        CADIZ MIX! &copy; Copyright 2024. Designed By Marvin Cadiz</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end copyright -->
+
+	</body>
+</html>
